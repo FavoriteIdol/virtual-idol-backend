88861f956732e54c471a176f3ee008b9fc4b1c9c
dudnjsckrgo <dudnjsckrgo@gmail.com>
2024-12-15 15:15:37 +0900

    error fix

--- src/main/java/com/outsider/virtual/concert/command/application/service/ConcertDeleteService.java
+++ src/main/java/com/outsider/virtual/concert/command/application/service/ConcertDeleteService.java
@@ -1,9 +1,9 @@
 package com.outsider.virtual.concert.command.application.service;
 
 import com.outsider.virtual.concert.command.domain.aggregate.Concert;
-import com.outsider.virtual.concert.command.domain.repository.ConcertCollectionRepository;
 import com.outsider.virtual.concert.command.domain.repository.ConcertRepository;
 import com.outsider.virtual.song.command.domain.repository.SongRepository;
+import com.outsider.virtual.useractivity.command.domain.repository.ConcertCollectionRepository;
 import com.outsider.virtual.user.exception.NotExistException;
 import com.outsider.virtual.user.exception.NotMineException;
 import org.springframework.stereotype.Service;
@@ -37,7 +37,7 @@ public class ConcertDeleteService {
         // concert_collection 테이블의 데이터를 먼저 삭제
         concertCollectionRepository.deleteByConcertId(id);
 
-        // 해당 콘서트에 속한 노래들을 삭제
+        // 해당 콘서트에 속�� 노래들을 삭제
         songRepository.deleteByConcertId(id);
         
         // 마지막으로 콘서트 삭제
